5dd8b025f3b3ae32a1e4fcd3f7c5dd8247a1190d
Gaspito <gaspard.dauvillier@gmail.com>
2025-03-10 10:00:33 -0400

    Updated book fetching routes to be asynchronous

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,7 +21,7 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
     const bookList = Object.keys(books).map(isbn => {
         const book = books[isbn];
         return {isbn, author: book.author, title: book.title};
@@ -30,7 +30,7 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
     const book = books[isbn];
     if (!book)
@@ -39,7 +39,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
     const booksByAuthor = Object.keys(books).map(isbn => {
         const book = books[isbn];
@@ -49,7 +49,7 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
     const booksWithTitle = Object.keys(books).map(isbn => {
         const book = books[isbn];
