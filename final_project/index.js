c132c07673231d9cf6efa97ce56ae9d876ce8d4a
Gaspito <gaspard.dauvillier@gmail.com>
2025-03-10 08:41:18 -0400

    Added auth route

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,20 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const sessionAuth = req.session.authorization;
+
+    if (!sessionAuth) 
+        return res.status(403).json({message: "User is not logged in"});
+
+    const {accessToken, username} = sessionAuth;
+    
+    jwt.verify(accessToken, "access", (err, user) => {
+        if (err)
+            return res.status(403).json({message: "Failed to authentificate user"});
+
+        req.user = user;
+        next();
+    });
 });
  
 const PORT =5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,17 +6,37 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    let validUsers = users.filter(u => {
+        return u.username === username;
+    });
+    return validUsers >= 1;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validUsers = users.filter(u => {
+        return u.username === username && u.password === password;
+    });
+    return validUsers >= 1;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (!username || !password) 
+        return res.status(404).json({message: "Missing login username or password."});
+
+    if (authenticatedUser(username, password))
+        return res.status(208).json({message: "Could not log in: invalid username or password."});
+
+    let accessToken = jwt.sign({
+        data: password,
+    }, "access", {expiresIn: 60 * 60 * 3});
+
+    req.session.authorization = {accessToken, username};
+
+    return res.status(200).json({message: "Log in successful"});
 });
 
 // Add a book review
